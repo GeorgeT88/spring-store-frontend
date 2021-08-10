5eedc8bfe36c3e9bb01b2c248be0e9728d31df96
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2021-08-10 10:18:41 +0300

    initial commit

---
+++ src/views/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class SignUp extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Sing Up</h2>
+            </div>
+        );
+    }
+}
+
+export default SignUp
