21edf512d73463c92fbc3fde03963a0992225c23
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2021-08-12 12:18:16 +0300

    SignUp added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React from "react";
-
-
-import ButtonAppBar from './views/ButtonAppBar';
+import SignUp from './views/SignUp';
 import SignIn from './views/SignIn';
-import "./App.css"
-
-
-
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-      <ButtonAppBar />
-      <SignIn />
-    </div>
-  );
+    <Router>
+        <Switch>
+          <Route exact path = "/"><SignIn/></Route>
+          <Route path = "/signUp"><SignUp/></Route>
+        </Switch>
+    </Router>
+   );
 
 }
 
--- src/views/SignUp.js
+++ src/views/SignUp.js
@@ -5,25 +5,20 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import {Link} from 'react-router-dom';
-import { useHistory } from 'react-router-dom';
-
-
-const axios = require('axios').default;
-const ACCESS_TOKEN = "access_token";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Spring Store App
+        Your Website
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -44,35 +39,16 @@ const useStyles = makeStyles((theme) => ({
   },
   form: {
     width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
+    marginTop: theme.spacing(3),
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
 }));
 
-function handleSignIn(username, password) {
-    axios.post('http://localhost:8762/login', {
-        username: username,
-        password: password
-    }).then((response) => {
-        localStorage.setItem(ACCESS_TOKEN, response.headers.authorization);
-        console.log(response.headers.authorization);
-    }).catch((error) => {
-        console.log(error)
-    })
-}
-
- function SignIn() {
+  function SignUp() {
   const classes = useStyles();
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  let history = useHistory();
-
-  const redirect = () => {
-    history.push('/SignUp')
-  }
-
+  
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -81,65 +57,85 @@ function handleSignIn(username, password) {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Sign in
+          Sign up
         </Typography>
         <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="username"
-            label="Username"
-            name="username"
-            autoComplete="username"
-            autoFocus
-            onChange={e => setUsername(e.target.value)}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            onChange={e => setPassword(e.target.value)}            
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid>
+          </Grid>
           <Button
             type="submit"
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
-            onClick={() => handleSignIn(username, password)}
           >
-            Sign In
+            Sign Up
           </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
+          <Grid container justifyContent="flex-end">
             <Grid item>
-              <Link href="#" variant="body2"  to= '/signUp'>
-                {"Don't have an account? Sign Up"}               
-              </Link>            
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
             </Grid>
           </Grid>
         </form>
       </div>
-      <Box mt={8}>
+      <Box mt={5}>
         <Copyright />
       </Box>
     </Container>
   );
 }
-export default SignIn;
+export default SignUp;
