150fde82f968dba11fec3ce76068f56d2e95d871
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2021-08-13 12:47:55 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import SignUp from './views/SignUp';
 import SignIn from './views/SignIn';
 import ForgotPassword from './views/ForgotPassword';
+import ProductCatalog from './views/ProductCatalog';
+import AppBar from './views/AppBar';
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,14 +12,18 @@ import {
 
 function App() {
   return (
+    <div className="App">
+    <AppBar />
+  
     <Router>
         <Switch>
-          <Route exact path = "/"><SignIn/></Route>
+        <Route exact path = "/"><ProductCatalog/></Route>
           <Route path = "/signUp"><SignUp/></Route>
           <Route path = "/signIn"><SignIn/></Route>
           <Route path = "/forgotPassword"><ForgotPassword/></Route>
         </Switch>
     </Router>
+    </div>
    );
 
 }
