ba8a61aa569927ffcc64e16267caec1ac8927450
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2021-08-17 09:48:44 +0300

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,67 +10,66 @@ import { makeStyles } from '@material-ui/core/styles';
 
 
 const useStyles = makeStyles((theme) => ({
-    icon: {
-      marginRight: theme.spacing(220),
-    },
-    heroContent: {
-      backgroundColor: theme.palette.background.paper,
-      padding: theme.spacing(2, 0, 2),
-    },
-    heroButtons: {
-      marginTop: theme.spacing(2),
-    },
-    cardGrid: {
-      paddingTop: theme.spacing(8),
-      paddingBottom: theme.spacing(8),
-    },
-    card: {
-      height: '100%',
-      display: 'flex',
-      flexDirection: 'column',
-    },
-    cardMedia: {
-      paddingTop: '56.25%', // 16:9
-    },
-    cardContent: {
-      flexGrow: 1,
-    },
-    footer: {
-      backgroundColor: theme.palette.background.paper,
-      padding: theme.spacing(6),
-    },
-  }));
+  icon: {
+    marginRight: theme.spacing(220),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(2, 0, 2),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(2),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
 const Product = (props) => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <Grid item key={props.card} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      View
-                    </Button>
-                    <Button size="small" color="primary">
-                    {props.card}
-                     
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid> 
-    )
+  return (
+    <Grid item key={props.card} xs={12} sm={6} md={4}>
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.cardMedia}
+          image="https://source.unsplash.com/random"
+          title="Image title"
+        />
+        <CardContent className={classes.cardContent}>
+          <Typography gutterBottom variant="h5" component="h2">
+            Heading
+          </Typography>
+          <Typography>
+            This is a media card. You can use this section to describe the content.
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" color="primary">
+            View
+          </Button>
+          <Button size="small" color="primary">
+            Add To Cart
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
+  )
 }
 
 export default Product;
