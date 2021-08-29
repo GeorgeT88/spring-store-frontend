ea78db60416b2507d09113b09fe498eb55b31fb8
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2021-08-29 12:55:41 +0300

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -45,33 +45,33 @@ const Product = (props) => {
 
   return (
     <Grid item key={props.card.id} xs={12} sm={6} md={4}>
-    <Card className={classes.card}>
-      <CardMedia
-        className={classes.cardMedia}
-        image="https://source.unsplash.com/random"
-        title="Image title"
-      />
-      <CardContent className={classes.cardContent}>
-        <Typography gutterBottom variant="h5" component="h2">
-          {props.card.productName}
-        </Typography>
-        <Typography>
-          Description: {props.card.productDescription}
-        </Typography>
-        <Typography gutterBottom variant="h6" component="h2">
-         Product price: {props.card.productPrice}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" color="primary">
-          View
-        </Button>
-        <Button size="small" color="primary">
-          Edit
-        </Button>
-      </CardActions>
-    </Card>
-  </Grid>
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.cardMedia}
+          image={props.card.productPhotoLink}
+          title="Image title"
+        />
+        <CardContent className={classes.cardContent}>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.card.productName}
+          </Typography>
+          <Typography>
+            {`Description: ${props.card.productDescription}`}
+          </Typography>
+          <Typography gutterBottom variant="h6" component="h2">
+            {`Product price:  ${props.card.productPrice}`}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" color="primary">
+            {`View`}
+          </Button>
+          <Button size="small" color="primary">
+            {`Add To Cart`}
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
   )
 }
 
