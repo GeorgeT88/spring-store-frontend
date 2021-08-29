d96b4527ec4562423ea508a8b15b71534abf7914
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2021-08-29 15:35:30 +0300

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -40,26 +40,27 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(6),
   },
 }));
-const Product = (props) => {
+const Product = (product) => {
   const classes = useStyles();
+  const {productPhotoLink,productName, productDescription,productPrice} = product;
 
   return (
     <Grid item xs={12} sm={6} md={4}>
       <Card className={classes.card}>
         <CardMedia
           className={classes.cardMedia}
-          image={props.productPhotoLink}
+          image={productPhotoLink}
           title="Image title"
         />
         <CardContent className={classes.cardContent}>
           <Typography gutterBottom variant="h5" component="h2">
-            {props.productName}
+            {productName}
           </Typography>
           <Typography>
-            {`Description: ${props.productDescription}`}
+            {`Description: ${productDescription}`}
           </Typography>
           <Typography gutterBottom variant="h6" component="h2">
-            {`Product price:  ${props.productPrice}`}
+            {`Product price:  ${productPrice}`}
           </Typography>
         </CardContent>
         <CardActions>
