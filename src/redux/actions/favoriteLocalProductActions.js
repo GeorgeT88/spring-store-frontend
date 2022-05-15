d65a18bcc0ea5f143e86d74c1579743ddf22e576
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2022-05-15 09:55:23 +0300

    Update favoriteLocalProductActions.js

--- src/redux/actions/favoriteLocalProductActions.js
+++ src/redux/actions/favoriteLocalProductActions.js
@@ -1,21 +1,26 @@
+import { toast } from "react-toastify";
+
 const ADD_PRODUCT_TO_FAVORITES_LIST = "ADD_PRODUCT_TO_FAVORITES_LIST";
 const REMOVE_PRODUCT_TO_FAVORITES_LIST = "REMOVE_PRODUCT_TO_FAVORITES_LIST";
 
 
 
 
-export const addProductToFavoritesLocal = (product) =>(
-    {
-      type: ADD_PRODUCT_TO_FAVORITES_LIST,
-      product:product
-  }
-  )
-  export const removeProductfromFavoritesLocal = (product) =>(
-    {
-      type: REMOVE_PRODUCT_TO_FAVORITES_LIST,
-      product:product
-  }
-  )
+export const addProductToFavoritesLocal = (product) => async (dispatch) => {
+  dispatch({
+    type: ADD_PRODUCT_TO_FAVORITES_LIST,
+    product: product
+  })
+  toast.success("Product Added To Local Favorites!", { position: "top-right" })
+}
+
+export const removeProductfromFavoritesLocal = (product) => async (dispatch) => {
+  dispatch({
+    type: REMOVE_PRODUCT_TO_FAVORITES_LIST,
+    product: product
+  })
+  toast.error("Product Removed from Local Favorites!", { position: "top-right" })
+}
 
 const initialState = {
   products: []
@@ -24,16 +29,16 @@ const initialState = {
 const favoriteProductActions = (state = initialState, action) => {
 
   switch (action.type) {
-    case ADD_PRODUCT_TO_FAVORITES_LIST:   
+    case ADD_PRODUCT_TO_FAVORITES_LIST:
       return {
         ...initialState,
         products: [...state.products, action.product]
       }
-      case REMOVE_PRODUCT_TO_FAVORITES_LIST:    
+    case REMOVE_PRODUCT_TO_FAVORITES_LIST:
       return {
         ...initialState,
         products: state.products.filter((product) => product.productName !== action.product.productName)
-      }  
+      }
     default:
       return state;
   }
